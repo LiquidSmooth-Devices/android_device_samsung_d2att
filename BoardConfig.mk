4784f268945ea509704d6108f1a51580de687774
f3n1xu5 <fenixus@gmail.com>
2015-02-04 19:10:23 -0500

    Adding d2can ASSERT line to cover install of Canadian I747M variants
    Change-Id: I12b9f0e9dd92641f552ed187d73a2964115b603d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,7 +27,6 @@
 -include vendor/samsung/d2att/BoardConfigVendor.mk
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := d2att,d2lte
-
+TARGET_OTA_ASSERT_DEVICE := d2att,d2lte,d2can
 TARGET_BOARD_INFO_FILE ?= device/samsung/d2att/board-info.txt
 
