362031ffc337e07bad411429cf5da5eda2adf328
f3n1xu5 <fenixus@gmail.com>
2015-04-22 11:29:00 -0400

    Updating build fingerprint to 5.1
    Change-Id: Iec9b990374ffb915a5561f802afff16b6123103d

--- liquid.mk
+++ liquid.mk
@@ -9,8 +9,8 @@ $(call inherit-product, vendor/liquid/config/common_phone.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=d2uc \
     TARGET_DEVICE=d2att \
-    BUILD_FINGERPRINT="samsung/d2uc/d2att:4.4.2/KOT49H/I747UCUFNE4:user/release-keys" \
-    PRIVATE_BUILD_DESC="d2uc-user 4.4.2 KOT49H I747UCUFNE4 release-keys"
+    BUILD_FINGERPRINT="samsung/d2uc/d2att:5.1.1/LMY47V/I747UCUFNE4:user/release-keys" \
+    PRIVATE_BUILD_DESC="d2uc-user 5.1.1 LMY47V I747UCUFNE4 release-keys"
 
 PRODUCT_NAME := liquid_d2att
 PRODUCT_DEVICE := d2att
